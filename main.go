1fd32dc0d81ab9937ced130f8225fc2d188702cf
Nikita Nafranets <eddimensi@gmail.com>
2017-08-13 20:03:29 +0300

    done tets

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"sort"
+	"strconv"
+)
+
 func main() {
 	println("Hi")
 }
@@ -24,7 +29,38 @@ func IntSliceToString(numbers []int) string {
 	var result string
 
 	for _, value := range numbers {
-		result += string(value)
+		result += strconv.Itoa(value)
+	}
+
+	return result
+}
+
+func MergeSlices(firstSlice []float32, secondSlice []int32) []int {
+	result := make([]int, 0, len(firstSlice)+len(secondSlice))
+
+	for _, value := range firstSlice {
+		result = append(result, int(value))
+	}
+
+	for _, value := range secondSlice {
+		result = append(result, int(value))
+	}
+
+	return result
+}
+
+func GetMapValuesSortedByKey(m map[int]string) []string {
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	sort.Ints(keys)
+
+	result := make([]string, 0, len(keys))
+
+	for _, key := range keys {
+		result = append(result, m[key])
 	}
 
 	return result
